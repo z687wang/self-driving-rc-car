9cdb4842fcd0d6d693f4c79ad48d883e93ebaff7
Zhe Wang <tonyzhe@foxmail.com>
2018-06-04 23:08:49 -0400

    temperature module upgrade

--- temperature.py
+++ temperature.py
@@ -1,17 +1,13 @@
-import RPi.GPIO as GPIO
-import dht11
+import sys
+import Adafruit_DHT
+import time
+import requests
+import json
 
-# initialize GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.cleanup()
+while True:
 
-# read data using pin 14
-instance = dht11.DHT11(pin = 16)
-result = instance.read()
-
-if result.is_valid():
-        print("Temperature: %d C" % result.temperature)
-        print("Humidity: %d %%" % result.humidity)
-else:
-        print("Error: %d" % result.error_code)
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    #data = {'humidity':humidity,'temperature':temperature}
+    #json_data = json.dumps(data)
+    #r = requests.post('http://192.168.0.8:1880/payload',json_data)
+    print(humidity,temperature)
